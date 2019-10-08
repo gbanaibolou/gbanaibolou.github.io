aec1a25d71bcbe559f8c6b87356d2613c9886eda
Gbanaibolou Burutolu <gbanaibolou@gmail.com>
2019-10-08 01:44:43 +0100

    Robot friends

---
+++ precache-manifest.a2bf69482a9ce117d7efa48efdfceade.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "110d66d127523414a0b5445c28e309f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "525cbf141fa9b566772c",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b03c9043bdfa4ea49643",
+    "url": "/static/css/main.4fef0f8e.chunk.css"
+  },
+  {
+    "revision": "525cbf141fa9b566772c",
+    "url": "/static/js/2.4cc39249.chunk.js"
+  },
+  {
+    "revision": "b03c9043bdfa4ea49643",
+    "url": "/static/js/main.ebb70682.chunk.js"
+  },
+  {
+    "revision": "da1149f62c2285d31cfa",
+    "url": "/static/js/runtime-main.94d5770c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
